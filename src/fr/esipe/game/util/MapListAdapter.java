109ae979fba30a8a72fea53bf13710b6055b747b
ANDREOTTI Benjamin <benjamin.andreotti@gmail.com>
2013-05-27 11:33:58 +0200

    Move and start in %

--- src/fr/esipe/game/util/MapListAdapter.java
+++ src/fr/esipe/game/util/MapListAdapter.java
@@ -55,13 +55,18 @@ public class MapListAdapter extends ArrayAdapter<MapLvl> {
 		}
 	}
 	
+	
+	public double getPourcent(int point, int size){
+		return (100*new Integer(point).doubleValue())/new Integer(size).doubleValue();
+	}
 	public boolean onTouch(View v, MotionEvent event) {
-
+	
 		if(MoveMode){
 			Log.d("SaveMOVE", "MOOOOOVE");
 			if(Math.abs(x-(int) event.getX())>10||Math.abs(y-(int) event.getY())>10)
 			{
-				addmoveonShip((int) event.getX(), (int) event.getY());
+				//addmoveonShip((int) event.getX(), (int) event.getY());
+				addmoveonShip(getPourcent((int) event.getX(), v.getWidth()), getPourcent((int) event.getY(), v.getHeight()));
 			}
 			if(event.getAction()==MotionEvent.ACTION_UP)
 				MoveMode=false;
@@ -87,14 +92,14 @@ public class MapListAdapter extends ArrayAdapter<MapLvl> {
 		public List<Coord> listmove=new LinkedList<Coord>();
 	}
 	private class Coord {
-		int x;
-		int y;
+		double x;
+		double y;
 	}
-	public void addmoveonShip(int x,int y){
+	public void addmoveonShip(double d,double e){
 		List<ShipOnMap>lship =ship.get(movemodepostion);
 		Coord coord = new Coord();
-		coord.x=x;
-		coord.y=y;
+		coord.x=d;
+		coord.y=e;
 		if(lship!=null){
 			lship.get(movemodeindex).listmove.add(coord);
 		}
@@ -110,8 +115,10 @@ public class MapListAdapter extends ArrayAdapter<MapLvl> {
 			ShipOnMap som= new ShipOnMap();
 			RelativeLayout fl = (RelativeLayout) v;
 			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(selectedShip.getWidth(), selectedShip.getHeight());
-			som.start.x=x;
-			som.start.y=y-v.getTop();
+			/*som.start.x=x;
+			som.start.y=y-v.getTop();*/
+			som.start.x=getPourcent(x, v.getWidth());
+			som.start.y=getPourcent(y-v.getTop(), v.getHeight());
 			som.time=(countitem-1-position)*3+1;
 			String[] shipinfo=((String) selectedShip.getTag()).split(":");
 			som.type=shipinfo[0];
